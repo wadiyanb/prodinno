b66b5363ed16456cb3c1efe9c0f452a450733623
wadiyanb <arzaanswadia@gmail.com>
2025-07-18 01:13:44 +0530

    bug

--- netlify/functions/next-runtime.js
+++
@@ -1,7 +0,0 @@
-// This file is required for Netlify Forms with Next.js 13+ and the new plugin
-exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: "OK"
-    };
-  };
