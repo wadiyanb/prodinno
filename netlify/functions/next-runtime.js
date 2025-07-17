1cafcf129d145f9506e35cbcf9ecdc63281dd6dc
wadiyanb <arzaanswadia@gmail.com>
2025-07-18 01:41:20 +0530

    removed

--- netlify/functions/next-runtime.js
+++
@@ -1,5 +0,0 @@
-// netlify/functions/next-runtime.js
-const { builder } = require('@netlify/functions');
-const { default: handler } = require('./.netlify/edge-functions/handler');
-
-exports.handler = builder(handler);
